bda32372eb7665800cf5455ae725ba65b7f99c59
issimissimo <d.suppo@issimissimo.com>
2021-04-29 16:49:51 +0200

    primissima implementazione firebase auth

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+export const config = {
+    apiKey: "AIzaSyCndxVzDZciBgcRpRXUdilFktBO7YkueYg",
+    authDomain: "marketplaceweb-68bd8.firebaseapp.com",
+    databaseURL: "https://marketplaceweb-68bd8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "marketplaceweb-68bd8",
+    storageBucket: "marketplaceweb-68bd8.appspot.com",
+    messagingSenderId: "519512003054",
+    appId: "1:519512003054:web:e784c30b66f765cbbb7eff"
+};
+
+
